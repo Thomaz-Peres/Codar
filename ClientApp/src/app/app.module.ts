2e5e23d6fef8ba9905d5937290e5b80e99814ff7
Thomaz Peres <boss@thomazperes.com>
2020-07-13 19:11:34 -0300

    Finish desktop

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 // import { RouterModule } from '@angular/router';
 
@@ -20,6 +20,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    ReactiveFormsModule,
     // HttpClientModule,
     // FormsModule,
     // RouterModule.forRoot([
